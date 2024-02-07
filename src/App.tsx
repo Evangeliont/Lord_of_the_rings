3d2761783ce77ff967c46efc23a770c5f5533e60
Dmitry Ulanov <ulanov2015ds@gmail.com>
2024-02-08 06:51:01 +0700

    dev : fix

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Content } from "./components/shared/Content/Content";
+import { Header } from "./components/shared/Header/Header";
+
+function App() {
+  return (
+    <Content>
+      <Header />
+    </Content>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.tsx";
+import App from "./App.tsx";
 import "./main.global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
