2deae7877e61c5426f7ddf2aee3aa2b69593fade
Dmitry <47912393+Evangeliont@users.noreply.github.com>
2024-02-08 16:47:36 +0700

    Revert "dev : write store, and API endpoints. Also components for render content"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { Content } from "./components/Content/Content";
 import { Header } from "./components/Header/Header";
-import { Layout } from "./components/Layout";
-import { CardList } from "./components/CardList";
 
 function App() {
   return (
     <Content>
       <Header />
-      <Layout>
-        <CardList />
-      </Layout>
     </Content>
   );
 }
